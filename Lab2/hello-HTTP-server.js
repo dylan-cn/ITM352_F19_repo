315c265158c21301dcc4d50099fefb30d0d30c51
Dylan <dylancn@hawaii.edu>
2019-09-05 13:27:55 -1000

    changed directory

--- Lab2/hello-HTTP-server.js
+++ Lab2/hello-HTTP-server.js
@@ -3,7 +3,7 @@ let http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
     console.log(req.headers); //output the request headers to the console
-    res.write('Hello World!'); //write a response to the client
+    res.write('<html><b>Hello World!</b></html>'); //write a response to the client
     res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
   
